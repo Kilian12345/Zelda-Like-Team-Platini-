15cacb3dc7a7b9938dffa7a083b071e141bbe229
Kilian-YES <allairekilian@gmail.com>
2019-02-28 17:40:32 +0100

    Ripple.Shader

---
+++ Prototype/Assets/Shader_Material/Tutorial_Scene/Ripple_Effect/Ripple.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Ripple : MonoBehaviour
+{
+    public Transform player;
+    Camera camera;
+    public Material RippleMaterial;
+    public float MaxAmount = 50f;
+
+    [Range(0, 1)]
+    public float Friction = .9f;
+
+    private float Amount = 0f;
+    public Vector3 pos;
+
+    void Update()
+    {
+        //pos.x = player.position.x;
+       // pos.y = player.position.y ;
+
+        if (Input.GetMouseButton(0))
+        {
+            this.Amount = this.MaxAmount;
+            Vector3 pos = Camera.main.WorldToScreenPoint(player.position); /////////////////////////////////////////// WARNING
+            this.RippleMaterial.SetFloat("_CenterX", pos.x);
+            this.RippleMaterial.SetFloat("_CenterY", pos.y);
+            Debug.Log (pos.x);
+            Debug.Log (pos.y);
+        }
+
+        this.RippleMaterial.SetFloat("_Amount", this.Amount);
+        this.Amount *= this.Friction;
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, this.RippleMaterial);
+    }
+}
