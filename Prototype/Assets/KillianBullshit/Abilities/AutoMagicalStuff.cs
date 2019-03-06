bda946c31d45c18ae348606d1d0cfe4e9218c83f
Kilian-YES <allairekilian@gmail.com>
2019-03-06 18:20:56 +0100

    Vsync

--- Prototype/Assets/KillianBullshit/Abilities/AutoMagicalStuff.cs
+++ Prototype/Assets/KillianBullshit/Abilities/AutoMagicalStuff.cs
@@ -24,26 +24,5 @@ public class AutoMagicalStuff
         EditorUtility.SetDirty(go.transform);
 
 
-        /* *
-        GameObject[] all;
-
-        all = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
-
-        foreach(GameObject go in all)
-        {
-            SomeFunctionalComponent script = go.GetComponent<SomeFunctionalComponent>();
-
-            if (script == null) continue;
-
-            script.self = go.transform;
-            script.speed = 18f;
-            // automatisation de tous les drag'n'drop imaginables ici
-
-            EditorUtility.SetDirty(script);
-        }
-
-        /* */
-
-        Debug.Log("done !");
     }
 }
