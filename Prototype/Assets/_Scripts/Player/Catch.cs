9407ede95dda7c50e0c203b069aadcbe53eb9cc5
Jay Sampat <jaydsampat@gmail.com>
2019-03-28 16:37:22 +0100

    Pickup and Throw
    Fixed this mech

--- Prototype/Assets/_Scripts/Player/Catch.cs
+++ Prototype/Assets/_Scripts/Player/Catch.cs
@@ -24,7 +24,7 @@ public class Catch : MonoBehaviour
                 }
                 if (Input.GetButtonDown("Fire1"))
                 {
-                    coll.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(moveHor, moveVer) * throwForce);
+                    coll.gameObject.GetComponent<Rigidbody2D>().velocity=(new Vector2(moveHor*throwForce, moveVer*throwForce));
                     coll.gameObject.transform.parent = null;
                     Invoke("isNotStuck", 0.05f);
 
