ad6324d0d93be1186db58062d587a4c881e95c9e
Kilian <k.allaire@rubika-edu.com>
2019-04-17 21:13:34 +0200

    Image Effect rage

---
+++ Prototype/Assets/Resources/Shader_Material/Tutorial_Scene/Rage_Visual/Rage_Visual.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Rage_Visual : MonoBehaviour
+{
+    [SerializeField] Material _material;
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+
+        Graphics.Blit(source, destination, _material);
+    }
+}
