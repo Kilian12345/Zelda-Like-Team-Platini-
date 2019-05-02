b0cf1c3613fd6941788bac64c48116cd45424a72
Kilian <k.allaire@rubika-edu.com>
2019-05-02 12:28:51 +0200

    New rage effect

---
+++ Prototype/Assets/R_Feedback/Shader_Material/Tutorial_Scene/Shaders/Rage_Visual/Glitch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glitch : MonoBehaviour
+{
+    [SerializeField] Material _material;
+public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+
+        Graphics.Blit(source, destination, _material);
+    }
+}
