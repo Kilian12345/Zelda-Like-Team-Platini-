be2ea4947dc37e27e2714553c49ca386d2acbd41
Kilian <k.allaire@rubika-edu.com>
2019-05-11 23:24:22 +0200

    90% Wait GA rework

--- Prototype/Assets/_Scripts/Map/Layering/Layer_Mouv.cs
+++ Prototype/Assets/_Scripts/Map/Layering/Layer_Mouv.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class Layer_Mouv : MonoBehaviour
 {
-    Vector3 referencePoint;
+    Transform referencePoint;
+    Transform player;
+    [SerializeField] Renderer[] rend;
+    
+
+    void Start()
+    {
+        referencePoint = GetComponent<Transform>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {   
+        for (int i = 0; i < rend.Length; i++)
+        {                   
+            if(referencePoint.position.y > player.position.y)
+            {rend[i].sortingOrder = 0;}
+            else
+            {rend[i].sortingOrder = 2;}
+        }
+    }
     
 }
