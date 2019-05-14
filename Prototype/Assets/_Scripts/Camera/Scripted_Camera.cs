92fbb185b6418f395c133dd534301ef52fd68856
Kilian <k.allaire@rubika-edu.com>
2019-05-15 01:27:52 +0200

    Cinematic_Tool 10%

---
+++ Prototype/Assets/_Scripts/Camera/Scripted_Camera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Scripted_Camera : MonoBehaviour
+{
+    FeedBack_Manager Fb_Mana;
+    Player plScript;
+    CinemachineVirtualCamera[] vcam; 
+
+    void Start()
+    {
+        Fb_Mana = GetComponent<FeedBack_Manager>();
+        plScript = FindObjectOfType<Player>();
+    }
+
+    void Update()
+    {
+        CameraSwitch();
+    }
+
+    void CameraSwitch()
+    {
+        for (int i = 0; i < vcam.Length; i++)
+        {
+            
+        }
+    }
+}
