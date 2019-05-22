ea241556b26e7c908cd9ce800cd76f067bcfd2ec
Kilian <k.allaire@rubika-edu.com>
2019-05-22 19:23:59 +0200

    push

---
+++ Prototype/Assets/_Scripts/Enemy/WAVE_SYSTEM.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WAVE_SYSTEM : MonoBehaviour
+{
+    [SerializeField] Vector3[] points;
+    [SerializeField] GameObject[] ennemyPrefab;
+    [SerializeField] float ennemiesInArena;
+    bool everyoneHasSpawn;
+
+    void FixedUpdate()
+    {
+        if (ennemiesInArena == 0)
+        {
+            for (int i = 0; i < ennemyPrefab.Length; i++)
+            {
+                
+            }
+        }
+    }
+}
