e67632a9daddd0f64c9715b32a24b9a878844a90
Kilian <k.allaire@rubika-edu.com>
2019-05-22 21:15:58 +0200

    wave systeme 50%

--- Prototype/Assets/_Scripts/Enemy/WAVE_SYSTEM.cs
+++ Prototype/Assets/_Scripts/Enemy/WAVE_SYSTEM.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class WAVE_SYSTEM : MonoBehaviour
 {
-    [SerializeField] Vector3[] points;
+    [SerializeField] Transform objectParent;
+    [SerializeField] Transform ennemyParent;
+    [SerializeField] Transform[] points;
     [SerializeField] GameObject[] ennemyPrefab;
-    [SerializeField] float ennemiesInArena;
-    bool everyoneHasSpawn;
+    [SerializeField] GameObject[] throwableObjects;
+    [SerializeField] List<Transform> ennemiesInArena = new List<Transform>();
+    public bool everyoneHasSpawn;
+    bool spawnMob;
+    bool spawnObj;
 
     void FixedUpdate()
     {
-        if (ennemiesInArena == 0)
+        if (everyoneHasSpawn==false)
         {
-            for (int i = 0; i < ennemyPrefab.Length; i++)
+
+            for (int i = 0; i < points.Length; i++)
             {
-                
+                float spawn = Random.Range(0,2);
+                GameObject ennemyGO;
+
+                if (spawn == 0)
+                {
+                    ennemyGO = Instantiate(ennemyPrefab[Random.Range(0, ennemyPrefab.Length)], points[i].position, Quaternion.identity, ennemyParent);
+                    ennemiesInArena.Add(ennemyGO.transform);
+                }
+                else
+                {Instantiate(throwableObjects[Random.Range(0, throwableObjects.Length)], points[i].position, Quaternion.identity, objectParent);}
             }
+            
+            everyoneHasSpawn = true;
         }
+
+        if (ennemiesInArena.Contains(null))
+        {
+            ennemiesInArena.Remove(null);
+        }
+        else if (ennemiesInArena.Count == 0)
+        {
+            everyoneHasSpawn = false;
+        }
+
     }
 }
