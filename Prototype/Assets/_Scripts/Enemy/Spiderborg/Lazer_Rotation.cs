3a653e1967330ee304b0f8ff560dfe2a6c71d550
Jay <jaydsampat@gmail.com>
2019-05-24 23:36:13 +0200

    Bug Fixes
    -Headbutt fix
    -Mini boss fix
    -Smoll fixes here and there

--- Prototype/Assets/_Scripts/Enemy/Spiderborg/Lazer_Rotation.cs
+++ Prototype/Assets/_Scripts/Enemy/Spiderborg/Lazer_Rotation.cs
@@ -6,52 +6,60 @@ public class Lazer_Rotation : MonoBehaviour
 {
     Fow_Parent fow;
     public GameObject LaserPoint;
-
     bool radiusIsGood;
 
-    float zAxis;
-    [SerializeField] float rotationSpeed = 200;
+    [SerializeField] float rotationspeed = 10;
+    float rotationleft = 360;
+
 
 
     void Start()
     {
         fow = GetComponentInChildren<Fow_Parent>();
-        zAxis = transform.rotation.z;
-
     }
 
 
     void Update()
     {
-        transform.position=LaserPoint.transform.position;
+        transform.position = LaserPoint.transform.position;
+        float rotation = rotationspeed * Time.deltaTime;
+
+
+
+        transform.position = LaserPoint.transform.position;
         if (fow.viewRadius >= fow.radius) { radiusIsGood = true; }
 
         if (fow.lazerActivated == true && radiusIsGood == true)
         {
-            StartCoroutine(Sale());
-
-            zAxis += Time.deltaTime;
-
-
-            transform.Rotate(0, 0, zAxis * rotationSpeed);
-
+            // StartCoroutine(Sale());
+            if (rotationleft > rotation)
+            {
+                rotationleft -= rotation;
+            }
+            else
+            {
+                rotation = rotationleft;
+                rotationleft = 0;
+                if (fow.lazerActivated == true)
+                {
+                    StartCoroutine(Sale());
+                }
+                    
+            }
+            transform.Rotate(0, 0, rotation);
         }
-        else
-        {
-            zAxis = 0;
-        }
-
     }
 
     IEnumerator Sale()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(1f);
 
         if(transform.localRotation.z < 2 && transform.localRotation.z > -10)
         {
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(0.1f);
             transform.rotation = Quaternion.Euler(0, 0, 0);
             fow.lazerActivated = false;
+            rotationleft = 360f;
         }
     }
 
