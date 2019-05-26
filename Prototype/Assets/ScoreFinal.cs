3755e1d67078361697d534ff78eb5ff66322740a
Anto <antonin.dartois@gmail.com>
2019-05-26 23:37:15 +0200

    Add Elevator End Score
    in Lvl 1 and 2

---
+++ Prototype/Assets/ScoreFinal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreFinal : MonoBehaviour
+{
+    [SerializeField] Text scoreDisplay;
+    Player ps;
+
+
+    void Start()
+    {
+        ps = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        scoreDisplay.text = ((int)ps.PlayerScore).ToString();
+    }
+}
